59559ebec224064333e883beb9a577b0a4be7af9
aswinr <aswinr019@gmail.com>
2023-03-30 22:16:25 +0530

    slight modifications

--- CO1/Program1.java
+++ CO1/Program1.java
@@ -29,29 +29,19 @@ class Program1{
     int price[] = new int[3];
 
     Scanner scanner = new Scanner(System.in);
+    Product[] product = new Product[3];
 
-    System.out.println("Enter the pcode , name , and price of the product one : ");
-    pcode[0] = Integer.parseInt(scanner.nextLine());
-    pname[0] = scanner.nextLine();
-    price[0] = Integer.parseInt(scanner.nextLine());
-    
-    System.out.println("Enter the pcode , name , and price of the product two : ");
-    pcode[1] = Integer.parseInt(scanner.nextLine());
-    pname[1] = scanner.nextLine();
-    price[1] = Integer.parseInt(scanner.nextLine());
+    for(int i = 0; i < 3 ; i++){
+      System.out.printf("Enter the pcode , name , and price of the product %d : ",i+1);
+      pcode[i] = Integer.parseInt(scanner.nextLine());
+      pname[i] = scanner.nextLine();
+      price[i] = Integer.parseInt(scanner.nextLine());
 
-    System.out.println("Enter the pcode , name , and price of the product three ");
-    pcode[2] = Integer.parseInt(scanner.nextLine());
-    pname[2] = scanner.nextLine();
-    price[2] = Integer.parseInt(scanner.nextLine());
-
-    Product p1 = new Product(pcode[0],pname[0],price[0]);
-    Product p2 = new Product(pcode[1],pname[1],price[1]);
-    Product p3 = new Product(pcode[2],pname[2],price[2]);
+      product[i] = new Product(pcode[i],pname[i],price[i]);
+    }
     Product lower;
 
-
-    lower = (p1.price < p2.price && p1.price < p3.price) ? p1: (p2.price < p1.price && p2.price < p3.price ) ? p2 : p3;
+    lower = (product[0].price < product[1].price && product[0].price < product[2].price) ? product[0]: (product[1].price < product[0].price && product[1].price < product[2].price ) ? product[1] : product[2];
 
     System.out.println("The product with lowest price is\n----------------------------- ");
     lower.display();
