aab28ee5a76049dbf5ad755bdb50b52857505bf7
aswinr <aswinr019@gmail.com>
2023-04-08 20:12:32 +0530

    added Program3 (co4)

---
+++ CO4/Program3/Program3.java
@@ -0,0 +1,70 @@
+/*
+ Write a user defined exception class to authenticate the username and password.
+ */
+
+import java.util.Scanner;
+
+
+class AuthenticationException extends Exception {
+  
+  public AuthenticationException(String message){
+    super(message);
+  }
+}
+
+class User {
+
+  private String username , password ;
+
+  User(String username , String password ){
+    this.username = username;
+    this.password = password;
+    System.out.println("account created!");
+  }
+
+  private boolean isValidUser(String username , String password ){
+
+    if(this.username.equals(username) && this.password.equals(password))
+      return true;
+    else
+      return false;
+  }
+
+  void login(String username , String password ) throws AuthenticationException {
+
+    if(!isValidUser(username,password))
+      throw new AuthenticationException("invalid username or password!");
+    else
+      System.out.println("logged-in successfully");
+  }
+
+}
+
+
+class Program3 {
+
+  public static void main(String args[]){
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("enter a username and password to create an account : ");
+    String username = scanner.next();
+    String password = scanner.next(); 
+
+    User user = new User(username,password);
+
+    System.out.print("enter the username and password to login : ");
+    username = scanner.next();
+    password = scanner.next();
+
+    try {
+      user.login(username,password);
+    }
+    catch( AuthenticationException e ){
+      System.out.println(e);
+    }
+    catch( Exception e ){
+      System.out.println("Something went wrong!");
+    }
+  }
+}
