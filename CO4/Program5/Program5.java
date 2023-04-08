bd977e9a3c44cfd09f7c71ce23e36683ada2a18e
aswinr <aswinr019@gmail.com>
2023-04-08 21:12:11 +0530

    added Program5 skelton (co4)

---
+++ CO4/Program5/Program5.java
@@ -0,0 +1,14 @@
+/* 
+ Define two classes. One for generating multiplication table of five and other for displaying first N prime numbers.
+ Implement using threads.(Thread class)*/
+
+
+import java.util.Scanner;
+
+
+public class Program5 {
+
+  public static void main(String args[]){
+
+  }
+}
