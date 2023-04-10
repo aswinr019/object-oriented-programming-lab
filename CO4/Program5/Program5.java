5e24747a97bda897783d0d94624795350d6dadb3
aswinr <2227@tkmce.ac.in>
2023-04-10 14:26:37 +0530

    minor changes

--- CO4/Program5/Program5.java
+++ CO4/Program5/Program5.java
@@ -1,10 +1,20 @@
 /* 
  Define two classes. One for generating multiplication table of five and other for displaying first N prime numbers.
- Implement using threads.(Thread class)*/
+ Implement using threads.(Thread class)
+ */
 
 
 import java.util.Scanner;
 
+class Multiplication {
+
+  Multiplication(){}
+}
+
+class PrimeNumber {
+
+  PrimeNumber(){}
+}
 
 public class Program5 {
 
