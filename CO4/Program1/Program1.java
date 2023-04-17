60cddb804c1f922d383ed89e2ee1f86d41855f7d
aswinr <2227@tkmce.ac.in>
2023-04-17 05:37:35 +0530

    Added communication capabilities b/w different clients

--- CO4/Program1/Program1.java
+++ CO4/Program1/Program1.java
@@ -18,19 +18,19 @@ class Program1{
 
       switch(choice){
 
-        case 0 : {
+        case 0 -> {
           System.out.println("exiting...");
-          break;
+ 
         }
-        case 1 : {
+        case 1 -> {
           System.out.print("enter the radius of the circle : ");
           double radius = scanner.nextDouble();
 
           Circle circle = new Circle(radius);
           System.out.printf("radius of the circle : %.2f\narea fo the circle : %.2f\n\n",circle.getRadius(),circle.getArea()); 
-          break;
+
         }
-        case 2 : {
+        case 2 -> {
           System.out.print("enter the base , height , side one , side two and side three of the triangle : ");
           double base = scanner.nextDouble();
           double height = scanner.nextDouble();
@@ -41,18 +41,18 @@ class Program1{
           Triangle triangle = new Triangle(base,height,side1,side2,side3);
 
           System.out.printf("base of the triangle : %.2f\nheight of the triangle : %.2f\nside one of the triangle : %.2f\nside two of the triangle : %.2f\nside three of the triangle : %.2f\narea of the triangle : %.2f\n\n",triangle.getBase(),triangle.getHeight(),triangle.getSideOne(),triangle.getSideTwo(),triangle.getSideThree(),triangle.getArea());
-          break;
+
         }
-        case 3 : {
+        case 3 -> {
           System.out.print("enter the side of the square : ");
           double side = scanner.nextDouble();
 
           Square square = new Square(side);
 
           System.out.printf("side of the square : %.2f\narea of the square : %.2f\n\n",square.getSide(),square.getArea());
-          break;
+
         }
-        case 4 : {
+        case 4 -> {
           System.out.print("enter the length and breadth of the rectangle : ");
           double length = scanner.nextDouble();
           double breadth = scanner.nextDouble();
@@ -62,7 +62,7 @@ class Program1{
           System.out.printf("length of the recatngle : %.2f\nbreadth of the rectangle : %.2f\narea of the rectangle : %.2f\n\n",rectangle.getLength(),rectangle.getBreadth(),rectangle.getArea());
           break;
         }
-        default : {
+        default -> {
           System.out.println("Wrong choice!");
         }
       }
--- client-server-communication-example/Client.java
+++ client-server-communication-example/Client.java
@@ -1,30 +1,62 @@
 import java.net.Socket;
 import java.io.*;
 
-
 public class Client {
 
   public static void main(String args[]) {
     
     try {
-
       System.out.println("Client started");
 
-      Socket socket = new Socket("localhost",3000);
+      Socket socket = new Socket("192.168.56.1", 3000);
+      System.out.println("Connected to server at " + socket.getInetAddress().getHostAddress() + ":3000");
 
       BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-
-      System.out.print("Enter a string : ");
-      String str = userInput.readLine();
-
-      PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-      out.println(str);
-      
       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      System.out.println(in.readLine());
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
+      while (true) {
+        System.out.print("Enter a string: ");
+        String str = userInput.readLine();
+        out.println(str);
+        System.out.println(in.readLine());
+      }
     } catch (Exception e){
       System.out.println(e.getMessage());
     }
   }
 }
+
+
+
+
+// import java.net.Socket;
+// import java.io.*;
+
+
+// public class Client {
+
+//   public static void main(String args[]) {
+    
+//     try {
+
+//       System.out.println("Client started");
+
+//       Socket socket = new Socket("localhost",3000);
+
+//       BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+//       System.out.print("Enter a string : ");
+//       String str = userInput.readLine();
+
+//       PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+//       out.println(str);
+      
+//       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//       System.out.println(in.readLine());
+
+//     } catch (Exception e){
+//       System.out.println(e.getMessage());
+//     }
+//   }
+// }
