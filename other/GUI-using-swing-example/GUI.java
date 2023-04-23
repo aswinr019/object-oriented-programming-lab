b7182f5d7eba6d336697dac82bd7c6eb751a31ff
aswinr <aswinr019@gmail.com>
2023-04-23 14:36:05 +0530

    minor additions

---
+++ other/GUI-using-swing-example/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+class GUI {
+    public static void main(String args[]) {
+      
+      JFrame frame = new JFrame();
+      frame.setTitle("Example frame");
+      frame.setResizable(false);
+      frame.setSize(420,420);
+      frame.setVisible(true);
+    }
+}
