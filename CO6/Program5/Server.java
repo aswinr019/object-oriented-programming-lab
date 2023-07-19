30406372a42adc00ad8be0a33cf15d6fc39c786a
aswinr <aswinr@noreply.com>
2023-07-19 19:50:03 +0530

    minor changes

--- CO6/Program5/Server.java
+++ CO6/Program5/Server.java
@@ -1,28 +1,59 @@
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
 
-public class Client {
+public class Server {
 
   public static void main(String args[]) {
-    
     try {
-      System.out.println("Client started");
+      System.out.println("Server started");
 
-      Socket socket = new Socket("127.0.0.1", 3000);
-      System.out.println("Connected to server at " + socket.getInetAddress().getHostAddress() + ":3000");
-
-      BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      ServerSocket serverSocket = new ServerSocket(3000);
+      InetAddress serverAddress = InetAddress.getLocalHost();
+      System.out.println("Server listening on " + serverAddress.getHostAddress() + ":3000");
 
       while (true) {
-        System.out.print("Enter a string: ");
-        String str = userInput.readLine();
-        out.println(str);
-        System.out.println(in.readLine());
+        Socket clientSocket = serverSocket.accept();
+        Thread thread = new Thread(new ClientHandler(clientSocket));
+        thread.start();
       }
-    } catch (Exception e){
+    } catch (Exception e) {
       System.out.println(e.getMessage());
     }
   }
 }
+
+class ClientHandler implements Runnable {
+  private Socket clientSocket;
+
+  public ClientHandler(Socket clientSocket) {
+    this.clientSocket = clientSocket;
+  }
+
+  public void run() {
+    try {
+      System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+      BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+      
+      String inputLine;
+      while ((inputLine = in.readLine()) != null) {
+        System.out.println("Received message from client " + clientSocket.getInetAddress().getHostAddress() + ": " + inputLine);
+        System.out.print("Enter a message to send to client " + clientSocket.getInetAddress().getHostAddress() + ": ");
+        String userMessage = userInput.readLine();
+        out.println("Server: " + userMessage);
+      }
+
+      System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
+      clientSocket.close();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
+
+
